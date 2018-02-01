121ceb1d9f4fdb74469561ffa13d5ffe44eff355
DESKTOP-96VA2DQ\노트북이요 <senariel.g@gmail.com>
2018-02-01 12:17:05 +0900

    공마다 큐볼과 닿았는지 판단할 변수 추가

--- 2DBilliard/Assets/Scripts/BallController.cs
+++ 2DBilliard/Assets/Scripts/BallController.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallController : MonoBehaviour {
+public class BallController : MonoBehaviour
+{
+    private bool bTouchedByCueBall;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        bTouchedByCueBall = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("CueBall"))
+            bTouchedByCueBall = true;
+    }
+
+    public void OnTurnStart()
+    {
+        bTouchedByCueBall = false;
+    }
+
+    public void OnTurnEnd()
+    {
+
+    }
+
+    public bool IsTouchedByCueBall()
+    {
+        return bTouchedByCueBall;
+    }
 }
